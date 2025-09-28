f2ff614cbb06041415f00eb338d43db45618bc84
2727-ask <ashutoshkumbhar27@gmail.com>
2025-09-27 20:35:52 -0700

    added home page

--- frontend/webapp/src/app/pages/home/home.ts
+++ frontend/webapp/src/app/pages/home/home.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
 })
-export class Home {
-
+export class Home implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
+  ngAfterViewInit() {
+    const script = this.renderer.createElement('script');
+    script.src = 'https://3d-parallax-effect.netlify.app/js/app.js';
+    script.type = 'text/javascript';
+    script.defer = true;
+    this.renderer.appendChild(document.body, script);
+  }
 }
