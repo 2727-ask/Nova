d6818a268c7089167a9b0f5869b7931485b31f48
PrathameshLakawade <prathameshlakawade@gmail.com>
2025-09-28 01:53:33 -0700

    Added user/analyze API

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.user import router as user_router
 from routers.statements import router as statements_router
+from routers.emissions import router as emissions_router
 from db.database import Base, engine
 from models import orm as orm_models
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Sustainable Financial Advisor")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],       # <- allow all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],       # <- allow all HTTP methods
-    allow_headers=["*"],       # <- allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.on_event("startup")
@@ -21,6 +22,8 @@ def on_startup():
 
 app.include_router(user_router, prefix="/user", tags=["User"])
 app.include_router(statements_router, prefix="/statements", tags=["Statements"])
+app.include_router(emissions_router, prefix="/emissions", tags=["Emissions"])
+
 
 @app.get("/", include_in_schema=False)
 def root():
@@ -28,11 +31,15 @@ def root():
         "status": "ok",
         "endpoints": {
             "/user": [
-                "/profile"
+                "/profile",
+                "/analyze"
             ],
             "/statements": [
                 "/categorize",
                 "/save"
+            ],
+            "/emissions": [
+                "/categorize/{statement_id}"
             ]
         }
     }
